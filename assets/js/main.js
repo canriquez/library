6242a7490127d759189a8b852e68ca0791b1b70c
Carlos Anriquez <canriquez@gmail.com>
2020-06-19 13:18:23 -0300

    add basic form html/css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 let myLibrary = [];
+
+function Book() {
+}
+
+Book.prototype.addBookToLibrary = function (name, author, read, summary) {
+    this.name = name;
+    this.author = author;
+    this.summary = summary;
+    this.read = read;
+    let label = "book:" + this.name + ", author:" + this.author;
+    label += ", summary:" + this.summary;
+    if (this.read) {
+        label += ", yes I have read this book";
+    } else {
+        label += ", not read";
+    }
+};
